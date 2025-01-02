f5adaf2b6081f6090d2b4cf0a4aa9c209911c3fc
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-02 11:42:21 -0500

    FELIZ 2025 NEW

--- src/New2025/NewYear.java
+++ src/New2025/NewYear.java
@@ -7,22 +7,24 @@ public class NewYear {
     public static void main(String[] args) throws InterruptedException {
         int year = LocalDate.now().getYear();
 
-        boolean noRendirse = true;
+        boolean rendirse = false;
 
         if (year == 2025) {
+
             System.out.println("¡Bienvenido al 2025!");
-            System.out.println("Que este año esté lleno de éxitos, alegría y grandes logros para ti.");
-            System.out.println(
-                    "Aprovecha cada día al máximo con actividades como viajar, disfrutar, y programar mucho. 🚀");
+            System.out.println("🙏 En este 2025, con Dios todo es posible.");
+            System.out.println("💪 Con Su ayuda, nada es imposible.");
+            System.out.println("¡Bendiciones y fuerza para este año! 🙌");
+
+            System.out.println("Que este año esté lleno de éxitos y alegría. 🚀");
 
             for (int dia = 1; dia <= 365; dia++) {
                 System.out.println("Día " + dia + " del 2025: ");
-                System.out.println("- 🌍 ¡Viaja y descubre nuevos lugares!");
-                System.out.println("- 😊 Disfruta de los momentos con tus seres queridos.");
-                System.out.println("- 💻 Dedica tiempo a programar y aprender algo nuevo.");
-                System.out.println("- 🎯 Recuerda: cada día es una nueva oportunidad para crecer.");
+                System.out.println("- 🌍 ¡Viaja y disfruta de la vida!");
+                System.out.println("- 😊 ¡Haz que cada momento cuente!");
+                System.out.println("- 💻 Sigue aprendiendo y creciendo. ");
 
-                if (noRendirse) {
+                if (!rendirse) {
                     System.out.println("- 💪 ¡No te rindas! Estás en el camino correcto.");
                 } else {
                     System.out.println("- 😓 ¡Recuerda que todo es parte del proceso! No te rindas.");
@@ -32,8 +34,9 @@ public class NewYear {
                 TimeUnit.HOURS.sleep(24);
             }
 
-            if (noRendirse) {
+            if (!rendirse) {
                 System.out.println("¡Has llegado al final del 2025 sin rendirte! 🎉");
+                System.out.println("Recuerda, Dios tiene un propósito para ti. 💪");
             } else {
                 System.out
                         .println("Recuerda, el 2025 estuvo lleno de lecciones, ¡sigue adelante en el próximo año! 💪");
